7df411ab47893e77df83a1f4ca205198160c6158
Caio Teodoro <caiorodrigot@hotmail.com>
2020-08-11 23:45:45 -0300

    Initial Commit

---
+++ Cost Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cost Calculator
+//
+//  Created by Caio Teodoro on 11/08/20.
+//  Copyright © 2020 Teodoro Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
